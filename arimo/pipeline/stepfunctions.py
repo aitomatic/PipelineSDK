e52b44c6240ed4165d9896876cb0018ab8986d0c
An Phan <phanhongan@gmail.com>
2020-04-02 15:58:23 -0700

    Allow to delete step function & exec a step function

--- arimo/pipeline/stepfunctions.py
+++ arimo/pipeline/stepfunctions.py
@@ -7,6 +7,15 @@ from arimo.pipeline.utils import load_yaml
 
 client = boto3.client('stepfunctions')
 
+STATE_MACHINE_ARN_PREFIX = "arn:aws:states:ap-northeast-1:394497726199:stateMachine:"
+ROLE_ARN = "arn:aws:iam::394497726199:role/pipeline-sdk-states-role"
+
+
+def write_json(filename, data):
+    with open(filename, 'w') as outfile:
+        json.dump(json.loads(data), outfile, indent=2)
+    print("JSON file:", filename)
+
 
 @cli.group()
 def sfn():
@@ -15,23 +24,59 @@ def sfn():
 
 @sfn.command()
 @click.option('--file', prompt='File')
-def create(file):
+@click.option('--out-json/--no-out-json', default=False)
+def create(file, out_json):
     o = load_yaml(file)
+    definition = json.dumps(o['definition'], indent=2)
     response = client.create_state_machine(
         name=o.get('name'),
-        definition=json.dumps(o['definition']),
-        roleArn='arn:aws:iam::394497726199:role/pipeline-sdk',
+        definition=definition,
+        roleArn=ROLE_ARN,
+    )
+    print(response)
+    if out_json:
+        write_json(file.replace('.yaml', '.json'), definition)
+
+
+@sfn.command()
+@click.option('--file', prompt='File')
+@click.option('--out-json/--no-out-json', default=False)
+def update(file, out_json):
+    o = load_yaml(file)
+    state_machine_arn = STATE_MACHINE_ARN_PREFIX + o['name']
+    definition = json.dumps(o['definition'], indent=2)
+    response = client.update_state_machine(
+        stateMachineArn=state_machine_arn,
+        definition=definition,
+    )
+    print(response)
+    if out_json:
+        write_json(file.replace('.yaml', '.json'), definition)
+
+
+@sfn.command()
+@click.option('--name', prompt='Name')
+def delete(name):
+    state_machine_arn = STATE_MACHINE_ARN_PREFIX + name
+    response = client.delete_state_machine(
+        stateMachineArn=state_machine_arn
     )
     print(response)
 
 
 @sfn.command()
-@click.option('--file', prompt='File')
-def update(file):
-    o = load_yaml(file)
-    state_machine_arn = "arn:aws:states:ap-northeast-1:394497726199:stateMachine:" + o['name']
-    response = client.update_state_machine(
+@click.option('--name', prompt='Name')
+@click.option('--input-file')
+def start(name, input_file):
+    state_machine_arn = STATE_MACHINE_ARN_PREFIX + name
+    input_json = {}
+    if input_file:
+        with open(input_file) as f:
+            input_json = json.load(f)
+    input_str = json.dumps(input_json, indent=2)
+
+    response = client.start_execution(
         stateMachineArn=state_machine_arn,
-        definition=json.dumps(o['definition']),
+        input=input_str
     )
     print(response)
